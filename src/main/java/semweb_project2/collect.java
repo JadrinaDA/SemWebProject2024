ecab9ba88d7bba8a22665f9c856f2a42b519e8fe
JadrinaDA <jadrinadeandrade@uc.cl>
2023-12-08 15:00:05 +0100

    Data collected now corresponds to restaurants, added all project to be safe

---
+++ src/main/java/semweb_project2/collect.java
@@ -0,0 +1,184 @@
+package semweb_project2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.vocabulary.RDF;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class collect {
+	
+	public static void main(String args[])
+	{
+		JSONArray o;
+		Model model = ModelFactory.createDefaultModel();
+		String schema = "https://schema.org/";
+		int counter = 0;
+		
+		try {
+			o = (JSONArray) new JSONParser().parse(new FileReader("coopcycle.json"));
+			//for (int i = 0; i < o.size(); i++)
+			for (int i = 0; i < 3; i++)
+			{
+				JSONObject service = (JSONObject) o.get(i);
+				JSONObject text = (JSONObject) service.get("text");
+				String ccurl = "";
+				if (service.get("coopcycle_url") != null)
+				{
+					ccurl = (String) service.get("coopcycle_url");
+				}
+				else if (service.get("delivery_form_url") != null){
+					ccurl = (String) service.get("delivery_form_url");
+				}
+				else
+				{
+					ccurl = (String) service.get("url");
+				}
+				if (ccurl == null)
+				{
+					continue;
+				}
+				
+				try {
+		            // Replace the URL with the actual URL of the website you want to parse
+		            Document document = Jsoup.connect(ccurl).get();
+
+		            // Select all div elements with class "restaurant-item"
+		            Elements restaurantItems = document.select("div.restaurant-item>a[href]");
+		            Set<String>setRI = new HashSet();
+		            for (Element restaurantHref: restaurantItems)
+		            {
+		            	setRI.add(ccurl + restaurantHref.attr("href"));
+		            }
+
+		            // Iterate over each div element
+		            for (String href : setRI) {
+		                // Select the 'a' element within each div and get its 'href' attribute
+		                
+		            	//System.out.println(href);
+		            	try {
+		                        Document restaurantDocument = Jsoup.connect(href).get();
+		                        Elements jsonScripts = restaurantDocument.select("script[type=application/ld+json]");
+		                        
+		                        //System.out.println(jsonScripts.size());
+
+		                        for (Element jsonScript : jsonScripts) {
+		                        	JSONParser parser = new JSONParser();
+		                        	JSONObject jsonText = (JSONObject) parser.parse(jsonScript.data());
+		                            //System.out.println("JSON Object: " + jsonText);
+		                            Resource address = model.createResource()
+		                                    .addProperty(RDF.type, model.createResource(schema+"PostalAddress"))
+		                                    .addProperty(model.createProperty(schema+"addressLocality"), (String) service.get("city"))
+		                                    .addProperty(model.createProperty(schema+"addressCountry"), ((String) service.get("country")).toUpperCase());
+		                            Double lat =(Double) ((JSONObject)((JSONObject) jsonText.get("address")).get("geo")).get("latitude");
+		                            Double lon =(Double) ((JSONObject)((JSONObject) jsonText.get("address")).get("geo")).get("longitude");
+		            				Resource location = model.createResource()
+		            						                 .addProperty(RDF.type, model.createResource(schema+"Place"))
+		            						                 .addProperty(model.createProperty(schema+"address"), address)
+		            						                 .addProperty(model.createProperty(schema+"latitude"), model.createTypedLiteral(lat))
+		            						                 .addProperty(model.createProperty(schema+"longitude"),model.createTypedLiteral(lon));
+		            			
+		            				
+		            				String name = (String) ((JSONObject) jsonText.get("address")).get("name");
+		            				String phone = (String) ((JSONObject) jsonText.get("address")).get("telephone");
+		            				JSONArray openingHoursArray = (JSONArray) jsonText.get("openingHoursSpecification");
+
+		            				Resource serv = model.createResource(href)
+								             .addProperty(RDF.type, model.createResource(schema+"ProfessionalService"))
+								             .addProperty(model.createProperty(schema+"location"), location)
+								             .addProperty(model.createProperty(schema+"name"), name)
+		            						 .addProperty(model.createProperty(schema+"telephone"), phone);
+		            				
+		            				for (Object openingHoursObject : openingHoursArray) {
+		                                JSONObject openingHours = (JSONObject) openingHoursObject;
+		                                List<String> days = (List<String>) openingHours.get("dayOfWeek");
+		                                String opens = (String) openingHours.get("opens");
+		                                String closes = (String) openingHours.get("closes");
+
+		                                // Format opening hours for each day
+		                                String oH = "";
+		                                for (String day : days) {
+		                                    oH += day.substring(0, 2) + ",";
+		                                }
+		                                String formattedHours = String.format("%s %s-%s", oH , opens, closes);
+		                                serv.addProperty(model.createProperty(schema+"openingHours"), formattedHours);
+		                            }
+		            				
+		                           
+		                            
+		            				
+		            				counter += 1;
+		                        }
+		                    } catch (IOException e) {
+		                        e.printStackTrace();
+		                 }
+//		            	Elements anchorElements = restaurantItem.select("a[href]");
+//		                for (Element anchorElement : anchorElements) {
+//		                    String href = anchorElement.attr("href");
+//		                    if (href.contains("restaurant") && !href.contains("restaurants"))
+//		                    {
+//		                    	System.out.println(ccurl+href);
+//		                    }
+//		                    //System.out.println("Href: " + piece);
+//		                }
+		            }
+		        } catch (IOException e) {
+		            //e.printStackTrace();
+		            continue;
+		        }
+				
+			
+						             
+				
+				//System.out.println(text);
+									 
+									 
+				                     //.addProperty(model.createProperty(schema+"url"), (String) service.get("url"))
+				                     //.addProperty(model.createProperty(schema+"url"), (String) service.get("facebook_url"));
+				
+			}
+			
+	        model.setNsPrefix("schema", schema);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		model.write(System.out, "Turtle") ;
+		String datasetURL = "http://localhost:3030/coopcycle_dataset";
+		String sparqlEndpoint = datasetURL + "/sparql";
+		String sparqlUpdate = datasetURL + "/update";
+		String graphStore = datasetURL + "/data";
+		RDFConnection conneg = RDFConnectionFactory.connect(sparqlEndpoint,sparqlUpdate,graphStore);
+		//conneg.load(model); // add the content of model to the triplestore	
+
+
+		
+	}
+	
+
+}
