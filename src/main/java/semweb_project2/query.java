a3a3889080d43252846006e0b3a4fc298e5f0b7f
NachitoPerez <irperez@fi.uba.ar>
2023-12-28 12:52:03 -0300

    feat: sort query result by distance

--- src/main/java/semweb_project2/query.java
+++ src/main/java/semweb_project2/query.java
@@ -17,7 +17,7 @@ public class query {
 		
 		// Create a query string
         String queryString = "PREFIX schema: <https://schema.org/> " +
-                             "SELECT ?storeUri ?storeName " +
+                             "SELECT ?storeName " +
                              "WHERE {" +
                              "  ?storeUri a schema:ProfessionalService ;" +
                              "            schema:openingHoursSpecification [ " +
@@ -26,27 +26,28 @@ public class query {
                              "              schema:closes ?closes ;" +
                              "              schema:dayOfWeek ?dayOfWeek" +
                              "            ] ;" +
-                             "schema:location [" +
+                             "            schema:location [" +
                              "                 schema:longitude ?long;" +
                              "                 schema:latitude ?lat;" +
-                             "             ];" +
+                             "            ] ;" +
                              "            schema:name ?storeName ;" +
                              "            schema:priceRange ?price ." +
                              "  FILTER (?dayOfWeek = \"userDayOfWeek\") " +
                              "  FILTER(hours(?closes) > userHour || (hours(?closes) = userHour && minutes(?closes) > userMinute) )" +
                              "  FILTER(hours(?opens) < userHour || (hours(?opens) = userHour && minutes(?opens) < userMinute) )" +
-                             "  FILTER (((?lat - userLat)*110.574)*((?lat - userLat)*110.574) +  ((?long - userLon)*111.32*cos)*((?long - userLon)*111.32*0.75)  < disSq)" +
+                             "  FILTER (((?lat - userLat)*110.574)*((?lat - userLat)*110.574) + ((?long - userLon)*111.32*cos)*((?long - userLon)*111.32*0.75)  < disSq)" +
                              "  FILTER (?price <= userLim)" +
-                             "} ORDER BY ?storeName";
+                             "} ORDER BY (((?lat - 49.40391)*110.574)*((?lat - 49.40391)*110.574) +  ((?long - 2.42788)*111.32*0.6507224012742214)*((?long - 2.42788)*111.32*0.75))";
 		
 		String datasetURL = "http://localhost:3030/coopcycle_dataset";
 		String sparqlEndpoint = datasetURL + "/sparql";
 		String sparqlUpdate = datasetURL + "/update";
 		String graphStore = datasetURL + "/data";
 		
+		UserProfile user = readpref.getUserPreferences();
+		
 		Scanner scan = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Enter day of week as number:");
-
+	    System.out.println("Welcome back " + user.getUserName() + ". Please, enter day of week as number:");
 	    String day = scan.nextLine();  // Read user input
 	    System.out.println("Enter time (hh:mm)");
 	    String time = scan.nextLine();
@@ -55,22 +56,10 @@ public class query {
 		String userHour = time.split(":")[0];
 		String userMinute = time.split(":")[1];
 		System.out.println("Looking up restaurant for: " + userDayOfWeek + " at " + time);  // Output user input
-		
-		System.out.println("Enter your latitude (float)");
-	    String templat = scan.nextLine();
-	    System.out.println("Enter your longitude (float)");
-	    String templon = scan.nextLine();
-		Double userLat = Double.parseDouble(templat);
-		Double userLon = Double.parseDouble(templon);
 		// We made the following assumptions based on an internet search and testing on google maps
 		// 1 degree of lat = 110.547
 		// 1 degree of lon = 111.32 * cos(lat)
-		System.out.println("Enter maximum distance/radius");
-	    String temprad = scan.nextLine();
-		Integer radius = Integer.parseInt(temprad);
-		Double cos = Math.cos(userLat * Math.PI /180);
-		System.out.println("Enter maximum price");
-		String userLim = scan.nextLine();
+		Double cos = Math.cos(user.getLatitude() * Math.PI /180);
 		scan.close();
 		
 		RDFConnection conn = RDFConnectionFactory.connect(sparqlEndpoint,sparqlUpdate,graphStore);
@@ -78,19 +67,18 @@ public class query {
         queryString = queryString.replaceAll("userDayOfWeek", userDayOfWeek);
         queryString = queryString.replaceAll("userHour", userHour);
         queryString = queryString.replaceAll("userMinute", userMinute);
-        queryString = queryString.replaceAll("userLat", Double.toString(userLat));
-        queryString = queryString.replaceAll("userLon", Double.toString(userLon));
+        queryString = queryString.replaceAll("userLat", Double.toString(user.getLatitude()));
+        queryString = queryString.replaceAll("userLon", Double.toString(user.getLongitude()));
         queryString = queryString.replaceAll("cos", Double.toString(cos));
-        queryString = queryString.replaceAll("disSq", Integer.toString(radius^2));
-        queryString = queryString.replaceAll("userLim", userLim);
+        queryString = queryString.replaceAll("disSq", Double.toString(Math.pow(user.getGeoRadius(),2)));
+        queryString = queryString.replaceAll("userLim", Double.toString(user.getMaxPrice()));
         QueryExecution qExec = conn.query(queryString) ;
 		ResultSet rs = qExec.execSelect() ;
 		while(rs.hasNext()) {
-		  QuerySolution qs = rs.next() ;
-		  Resource subject = qs.getResource("storeUri") ;
-		  System.out.println("Subject: " + subject) ;
+			QuerySolution soln = rs.nextSolution();
+	        String storeName = soln.getLiteral("storeName").getString();
+	        System.out.println("Store open: " + storeName);
 		}
-		System.out.println("Query finished") ;
 		qExec.close() ;
 		conn.close() ;
 
